bf363a5b9e5f15468076c23649222641be8e8f06
robced <117061812+robced@users.noreply.github.com>
2023-01-21 23:36:41 -0800

    fix: add session provider and context wrappers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {StateContext} from "@/context/StateContext";
+import {SessionProvider} from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <SessionProvider>
+            <StateContext>
+                <Component {...pageProps} />
+            </StateContext>
+        </SessionProvider>
+    );
 }
