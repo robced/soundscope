b1c503bbd71b3444a9c7db190c48375a699e142a
robced <117061812+robced@users.noreply.github.com>
2023-01-21 23:31:28 -0800

    feat: create player list

---
+++ components/PlayerList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Player from "@/components/Reusables/Player";
+
+const PlayerList = () => {
+    const playersArr = [
+        {
+            id: 0,
+            name: "timeline",
+            image: false,
+            background: false,
+            timeline: true,
+        }, {
+            id: 1,
+            name: "minimal",
+            image: false,
+            background: false,
+            timeline: false,
+        }, {
+            id: 2,
+            name: "cover",
+            image: true,
+            background: false,
+            timeline: false,
+        }, {
+            id: 3,
+            name: "background",
+            image: false,
+            background: true,
+            timeline: true,
+        }
+    ];
+
+    return (
+        <div className="w-full h-full grid grid-cols-3">
+            {playersArr.map(({id, name, image, background, timeline
+            }) => (
+                <Player key={id} text={name} image={image} background={background} timeline={timeline} />
+            ))}
+        </div>
+    );
+};
+
+export default PlayerList;
