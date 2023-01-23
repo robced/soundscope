7e0b06f1fe61e04ef8e25d6e9b037e7a6c0791e4
robced <117061812+robced@users.noreply.github.com>
2023-01-22 16:13:55 -0800

    feat: add song details types

--- interfaces/AppContext.interface.ts
+++ interfaces/AppContext.interface.ts
@@ -10,6 +10,16 @@ export interface AppContext {
     setAppData: Dispatch<SetStateAction<LanguageData>>;
     showLanguageMenu: Boolean;
     setShowLanguageMenu: Dispatch<SetStateAction<Boolean>>;
+    songName: String;
+    setSongName: Dispatch<SetStateAction<String>>
+    songImageLink: String;
+    setSongImageLink: Dispatch<SetStateAction<String>>
+    artistName: String;
+    setArtistName: Dispatch<SetStateAction<String>>
+    songLength: String;
+    setSongLength: Dispatch<SetStateAction<String>>
+    songTimeLeft: String;
+    setSongTimeLeft: Dispatch<SetStateAction<String>>
 }
 
 export interface ContextProps extends PropsWithChildren {
